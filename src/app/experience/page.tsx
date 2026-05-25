67494f0d68f05371e8f7c4d115e87f84333992f5
pratiklokesh5 <pratiklokesh5@gmail.com>
2026-05-24 19:44:35 -0700

    deployment fix

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -13,6 +13,7 @@ export default function Experience() {
     <section className="page">
       <h1>Experience</h1>
 
+      {/* TerraEd */}
       <div className="card">
         <h2>Founder and Head Coach</h2>
         <p>TerraEd • June 2023 – Present</p>
